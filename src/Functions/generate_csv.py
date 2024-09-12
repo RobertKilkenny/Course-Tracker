5620e758c7fb21f37f8e141f08522aca4b18dc22
Robert Kilkenny <robert.kilkenny7@gmail.com>
2024-09-12 00:56:21 -0400

    Trying to use UI template file (experiment)

--- src/Functions/generate_csv.py
+++ src/Functions/generate_csv.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QPushButton
+from PySide2.QtWidgets import QWidget, QPushButton, QLineEdit, QFileDialog
 from utils.process_course_data import CourseList
 from utils.subwindow_widget import SubwindowWidget
 from utils.app_manager import AppManager
@@ -7,6 +7,14 @@ class GenerateCSV(SubwindowWidget):
     """Create the window to have the user make a new class."""
     def __init__(self, course_list: CourseList, app_manager: AppManager):
         super().__init__(course_list, app_manager)
+
+        self.file_dialog = QFileDialog(self)
+        self.file_dialog.setFileMode(QFileDialog.ExistingFile)
+        self.file_dialog.setNameFilter("CSV File (*.csv)")
+
+        find_file_option = QWidget()
+        
+        gen_file_option = QWidget()
 
         self.complete = False
         save_button = QPushButton("Save Class")
@@ -18,6 +26,11 @@ class GenerateCSV(SubwindowWidget):
         self.layout.addWidget(self.prompt)
         self.setLayout(self.layout)
 
+
+    def handle_find_file(self):
+        """Opens a file explorer window so that the user can lead the app
+        to where their existing csv file is."""
+        self.app_manager.emit_open_window_request(self.file_dialog)
 
     def handle_save(self):
         """Handle the save functionality."""
